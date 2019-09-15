6d409a7d6441d6f9bb922cd65db7176401f8a121
Vitor Cortez <vitor.a.cortez@gmail.com>
2019-09-15 11:37:08 -0300

    Update Main activity

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.pokedex
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +10,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pokemon = Pokemon.getPokemonFromJSON("pokemon.json", this)
+        val pokemonList = Pokemon.getPokemonFromJSON("pokemon.json", this)
 
-        for (p in pokemon) {
-            Log.d("POKEMONS", p.toString())
+        val adapter = PokemonAdapter(this, pokemonList)
+        pokemon_list_view.adapter = adapter
+
+        pokemon_list_view.setOnItemClickListener { _, _, position, _ ->
+            val selectedPokemon = adapter.getItem(position) as Pokemon
+
+            val detailsIntent = PokemonDetailActivity.newIntent(this@MainActivity, selectedPokemon)
+            startActivity(detailsIntent)
         }
     }
 }
